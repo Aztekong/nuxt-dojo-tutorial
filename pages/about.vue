118a30bdbca8b8ffc7ff8c768e4d392ab9a3d56a
Diego Vazquez Soto <vazquez.draco@gmail.com>
2023-03-09 22:43:18 -0600

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est soluta unde iusto neque, 
+            saepe praesentium nesciunt reiciendis itaque! At cumque laboriosam dolorum magni qui tempore, 
+            nostrum reiciendis labore sed porro.</p>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est soluta unde iusto neque, 
+            saepe praesentium nesciunt reiciendis itaque! At cumque laboriosam dolorum magni qui tempore, 
+            nostrum reiciendis labore sed porro.</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    const { data } = await useFetch('/api/currency/MXN')
+</script>
+
+<style scoped>
+
+h2
+{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est soluta unde iusto neque, 
+            saepe praesentium nesciunt reiciendis itaque! At cumque laboriosam dolorum magni qui tempore, 
+            nostrum reiciendis labore sed porro.</p>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est soluta unde iusto neque, 
+            saepe praesentium nesciunt reiciendis itaque! At cumque laboriosam dolorum magni qui tempore, 
+            nostrum reiciendis labore sed porro.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+h2
+{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
